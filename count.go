8cbc9747fea6c30c7c627fdc0508e437f818824b
Witoo Harianto <witoo@outlook.com>
2015-04-02 18:02:38 +0700

    add count

---
+++ count.go
@@ -0,0 +1,40 @@
+package eneru
+
+type CountReq struct {
+	client *Client
+	index  string
+	ty     string
+}
+
+func NewCount(client *Client) *CountReq {
+	return &CountReq{
+		client: client,
+	}
+}
+
+func (req *CountReq) Index(index string) *CountReq {
+	req.index = index
+
+	return req
+}
+
+func (req *CountReq) Type(ty string) *CountReq {
+	req.ty = ty
+
+	return req
+}
+
+func (req *CountReq) Do() (int, error) {
+	resp, err := req.client.Request(GET, buildPathIndex(req.index, req.ty, "_count"), nil, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	ret := &CountResp{}
+	err = decodeResp(resp, ret)
+	return ret.Count, err
+}
+
+type CountResp struct {
+	Count int `json:"count"`
+}
---
+++ count_test.go
@@ -0,0 +1,43 @@
+package eneru
+
+import (
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type CountSuite struct {
+	suite.Suite
+	server *httptest.Server
+	client *Client
+}
+
+func TestCountSuite(t *testing.T) {
+	suite.Run(t, &CountSuite{})
+}
+
+func (t *CountSuite) SetupSuite() {
+	t.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		encodeResp(w, &CountResp{
+			Count: 10,
+		})
+	}))
+
+	client, err := NewClient(t.server.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	t.client = client
+}
+
+func (t *CountSuite) TearDownSuite() {
+	t.server.Close()
+}
+
+func (t *CountSuite) TestDo() {
+	count, err := t.client.Count().Index("test").Type("user").Do()
+	t.NoError(err)
+	t.Equal(10, count)
+}
