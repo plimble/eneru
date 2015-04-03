7e2704a2c46368854269a7974eec74c08ef68942
xier <xier.kokis@gmail.com>
2015-04-03 17:39:48 +0700

    change method name Id to ID, fix test response

--- index_test.go
+++ index_test.go
@@ -21,6 +21,10 @@ func TestIndexSuite(t *testing.T) {
 func (t *IndexSuite) SetupSuite() {
 	t.server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		encodeResp(w, &IndexResp{
+			Index:   "test",
+			Type:    "book",
+			ID:      "1",
+			Version: 1,
 			Created: true,
 		})
 	}))
@@ -45,7 +49,11 @@ func (t *IndexSuite) TestDo() {
 		j.AS("tags", "search", "computer")
 	})
 
-	resp, err := t.client.Index("test").Type("book").Body(j).Do()
+	resp, err := t.client.Index("test").Type("book").ID("1").Body(j).Do()
 	t.NoError(err)
+	t.Equal(resp.Index, "test")
+	t.Equal(resp.Type, "book")
+	t.Equal(resp.ID, "1")
+	t.Equal(resp.Version, 1)
 	t.True(resp.Created, true)
 }
