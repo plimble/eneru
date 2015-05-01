21ccae42e5726a96e87fdab2930d20c8491f85ae
Witoo Harianto <witoo@outlook.com>
2015-05-01 19:34:57 +0700

    update

--- mapping_create.go
+++ mapping_create.go
@@ -25,13 +25,13 @@ func (req *CreateMappingReq) Body(body *bytes.Buffer) *CreateMappingReq {
 	return req
 }
 
-func (req *CreateMappingReq) Do() (*CreateIndexResp, error) {
+func (req *CreateMappingReq) Do() (*CreateMappingResp, error) {
 	resp, err := req.client.Request(PUT, buildPath(req.index, "_mapping", req.ty), nil, req.body)
 	if err != nil {
 		return nil, err
 	}
 
-	ret := &CreateIndexResp{}
+	ret := &CreateMappingResp{}
 	err = decodeResp(resp, ret)
 	return ret, err
 }
