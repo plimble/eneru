459c9f16c5537988d716bfde9db0f332d7476d45
xier <xier.kokis@gmail.com>
2015-07-19 19:25:45 +0700

    add tsplitter function in util & test

--- utils.go
+++ utils.go
@@ -1,7 +1,9 @@
 package eneru
 
 import (
+	"bytes"
 	"encoding/json"
+	"github.com/plimble/tsplitter"
 	"github.com/plimble/utils/strings2"
 	"net/http"
 	"strings"
@@ -56,4 +58,29 @@ func buildPath(a ...string) string {
 	}
 
 	return string(b)
+}
+
+func splitString(dict *tsplitter.FileDict, data *bytes.Buffer) (*bytes.Buffer, error) {
+	var mapJson map[string]interface{}
+	err := json.Unmarshal(data.Bytes(), &mapJson)
+	if err != nil {
+		return nil, err
+	}
+
+	for index, value := range mapJson {
+		switch v := value.(type) {
+		case string:
+			mapJson[index] = strings.Join(tsplitter.Split(dict, value.(string)).All(), " ")
+		case []interface{}:
+			for i, n := range value.([]interface{}) {
+				str, ok := n.(string)
+				if !ok {
+					break
+				}
+				v[i] = strings.Join(tsplitter.Split(dict, str).All(), " ")
+			}
+		}
+	}
+	splitData, err := json.Marshal(mapJson)
+	return bytes.NewBuffer(splitData), err
 }
